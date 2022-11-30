aefdaf34ae5e5f89572f0e737e168d893cb02b39
Maggie Liu <maggie.j.liu@gmail.com>
2022-11-29 21:35:56 -0800

    start homepage

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("prettier-plugin-astro"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
