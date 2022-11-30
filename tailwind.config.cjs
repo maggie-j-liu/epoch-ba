0c43d914ea8e3e2d366d8b1d07cad901971abc2e
Maggie Liu <maggie.j.liu@gmail.com>
2022-11-30 10:01:01 -0800

    implement rest of design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fira: ["Fira Code", ...defaultTheme.fontFamily.mono],
+        mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        epoch: {
+          DEFAULT: "#FF4794",
+          light: "#FFA7CC",
+          lighter: "#FFD8E8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
